32e81e483b1f26596660182de5a4c63f94bf11b3
Gabie <gabriellenascimento1310@gmail.com>
2025-08-14 19:35:09 -0300

    Arrumado parcialmente as setas 14.08

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ btnDireita.addEventListener('click', () => {
 btnEsquerda.addEventListener('click', () => {
   carrossel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
 });
+
+
+const searchInput = document.querySelector('.search-bar');
+const cards = document.querySelectorAll('.card');
+
+function normalizeText(text) {
+  return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+}
+
+searchInput.addEventListener('input', function () {
+  const value = normalizeText(this.value);
+
+  cards.forEach(card => {
+    const titleElement = card.querySelector('.text');
+    const title = normalizeText(titleElement.textContent);
+    card.style.display = title.includes(value) ? 'block' : 'none';
+  });
+});
