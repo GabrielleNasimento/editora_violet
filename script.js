69fa295b9464d4652f594aa9c3b07aaa8c3a9e55
DherickOliveira <dherickballz@gmail.com>
2025-11-13 22:58:40 -0300

    Sobre nós e contate-nos rework

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+const carrossel = document.querySelector('.carrossel');
+const btnEsquerda = document.querySelector('.seta-esquerda');
+const btnDireita = document.querySelector('.seta-direita');
+
+const scrollAmount = 270;
+
+// Funções de scroll
+function scrollDireita() {
+  if (carrossel.scrollLeft + carrossel.offsetWidth >= carrossel.scrollWidth) {
+    carrossel.scrollTo({ left: 0, behavior: 'smooth' });
+  } else {
+    carrossel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  }
+}
+
+function scrollEsquerda() {
+  if (carrossel.scrollLeft <= 0) {
+    carrossel.scrollTo({ left: carrossel.scrollWidth, behavior: 'smooth' });
+  } else {
+    carrossel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  }
+}
+
+// Eventos de clique
+btnDireita.addEventListener('click', scrollDireita);
+btnEsquerda.addEventListener('click', scrollEsquerda);
+
+// Auto-scroll
+let autoScroll = setInterval(scrollDireita, 3000);
+
+// Pausar ao passar o mouse
+carrossel.addEventListener('mouseenter', () => {
+  clearInterval(autoScroll);
+});
+
+// Retomar quando o mouse sair
+carrossel.addEventListener('mouseleave', () => {
+  autoScroll = setInterval(scrollDireita, 3000);
+});
+
+// Barra
+
+
+const searchInput = document.querySelector('.search-bar');
+const cards = document.querySelectorAll('.card');
+
+function normalizeText(text) {
+  return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+}
+
+searchInput.addEventListener('input', function () {
+  const value = normalizeText(this.value);
+
+  cards.forEach(card => {
+    const titleElement = card.querySelector('.text');
+    const title = normalizeText(titleElement.textContent);
+    card.style.display = title.includes(value) ? 'block' : 'none';
+  });
+});
+
+const accordionTriggers = document.querySelectorAll('.accordion-trigger');
+  accordionTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+      const parentItem = trigger.parentElement;
+      parentItem.classList.toggle('active');
+    });
+  });
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const emailInput = document.getElementById("email");
@@ -17,3 +85,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
